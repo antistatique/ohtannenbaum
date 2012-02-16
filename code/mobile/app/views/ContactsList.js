aa6c0eb025b4abed09b50db7c3eedca3dec3934c
Mathieu Latin <mathieu.latin@gmail.com>
2012-02-16 14:04:31 +0100

    #5 vue de listing des contacts a ajouter au cercle courant

---
+++ code/mobile/app/views/ContactsList.js
@@ -0,0 +1,60 @@
+ot.views.ContactsList = Ext.extend(Ext.Panel, {
+    dockedItems: [{
+        xtype: 'toolbar',
+        title: 'Contacts'
+    }],
+    layout: 'fit',
+    items: [{
+      xtype: 'list',
+      store: ot.stores.contacts,
+      itemTpl: '{givenName} {familyName}',
+      grouped: true,
+      indexBar: true,
+      onItemDisclosure: function (record) {
+        console.log('record');
+        console.log(record);
+        var ActionSheetItems = new Array();
+        ActionSheetItems.push({
+          text : 'Cancel',
+          scope : this,
+          handler : function(record){
+            this.actions.hide();
+          }
+        });
+
+        if(record.data.emails.length > 0){
+          ActionSheetItems.push({
+            text: 'Utiliser ' + record.data.emails[0].value,
+            scope: this,
+            handler: function(record){
+              console.log('ajouter par email');
+            }
+          });
+        }
+
+        if(record.data.phoneNumbers.length > 0){
+          ActionSheetItems.push({
+            text: 'Utiliser ' + record.data.phoneNumbers[0].value,
+            scope: this,
+            handler: function(record){
+              console.log('ajouter par no de tel');
+            }
+          });
+        }
+
+        if (!this.actions) {
+          this.actions = new Ext.ActionSheet({
+            items: ActionSheetItems
+          });
+        }
+        this.actions.show();
+      }
+    }],
+    initComponent: function() {
+        ot.stores.contacts.load();
+        ot.views.ContactsList.superclass.initComponent.apply(this, arguments);
+    },
+    taptaptap: function(){
+      console.log('tap');
+    }
+});
