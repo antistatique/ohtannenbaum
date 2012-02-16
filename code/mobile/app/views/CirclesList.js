5a0d727042ffdf4b6d4948e4cc267a35ef743722
Mathieu Latin <mathieu.latin@gmail.com>
2012-02-16 14:01:06 +0100

    #3 Modification de la structure de la vue

--- code/mobile/app/views/CirclesList.js
+++ code/mobile/app/views/CirclesList.js
@@ -1,43 +1,47 @@
 ot.views.CirclesList = Ext.extend(Ext.Panel, {
-    dockedItems: [{
-        xtype: 'toolbar',
-        title: 'Circles',
-        items: [{
-            xtype: 'spacer'
-        }, {
-            xtype: 'button',
-            //text: "Add",
-            ui: 'plain',
-            iconMask: true,
-            iconCls: 'add',
-            handler: function() {
-                Ext.dispatch({
-                   controller: ot.controllers.circles,
-                   action: 'add' 
-                });
-            }
-        }]
-    }],
-    layout: 'fit',
-    items: [{
-        xtype: 'list',
-        store: Ext.getStore('ot.stores.Circle'),
-        itemTpl: '{title}',
-        listeners: {
-            itemtap: function (list, index, item, e) {
-                var record = list.getStore().getAt(index);
-                
-                Ext.dispatch({
-                    controller: ot.controllers.circles,
-                    action: 'show',
-                    id: record.getId()
-                });
-            }
+  styleHtmlContent: true,
+  scroll: 'vertical',
+  form: null,
+
+  initComponent: function() {
+    var that = this;
+    this.dockedItems = [{
+      xtype: 'toolbar',
+      title: 'Oh Tannenbaum',
+      record: null,
+      items: [
+        { xtype: 'spacer' },
+        {
+          xtype: 'button',
+          ui: 'plain',
+          iconMask: true,
+          iconCls: 'add',
+          handler: function() {
+            Ext.dispatch({
+              controller: ot.controllers.circles,
+              action: 'add' 
+            });
+          }
         }
-    }],
-    initComponent: function() {
-        
-        //ot.stores.circles.load();
-        ot.views.CirclesList.superclass.initComponent.apply(this, arguments);
-    }
+      ]
+    }];
+
+    this.items = [{
+      xtype: 'list',
+      store: Ext.getStore('ot.stores.Circle'),
+      itemTpl: '{title}',
+      listeners: {
+        itemtap: function (list, index, item, e) {
+          var record = list.getStore().getAt(index);
+          
+          Ext.dispatch({
+            controller: ot.controllers.circles,
+            action: 'show',
+            id: record.getId()
+          });
+        }
+      }
+    }];
+    ot.views.CircleCreate.superclass.initComponent.apply(this, arguments);
+  },
 });
